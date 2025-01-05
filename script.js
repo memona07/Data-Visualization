b68ce09a480c1b58f16cb0f8677cccc53d6deda7
memona07 <shopwithtalha@gmail.com>
2025-01-05 18:04:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var map = L.map('map').setView([31.5204, 74.3587], 5);
+var map = L.map('map').setView([31.5204, 72], 5);
 
+// Base layers
 var layer1 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
 var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -15,7 +15,7 @@ var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
     attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
 });
 
-//GEOJSON
+// GeoJSON layers
 var hospitalIcon = L.icon({
     iconUrl: 'hospitalll.png',
     iconSize: [30, 41],
@@ -29,110 +29,49 @@ var Hospitals = L.geoJSON(hospitals, {
         marker.bindPopup("<b>" + feature.properties["Name"] + "</b><br>" + feature.properties.Snippet);
         return marker;
     }
+});
+
+var pak = L.geoJSON(pakAd0, {
+    onEachFeature: function (feature, layer) {
+        if (feature.properties) {
+            layer.bindPopup(Object.keys(feature.properties)
+                .map(key => `<b>${key}:</b> ${feature.properties[key]}`)
+                .join('<br>'));
+        }
+    }
 }).addTo(map);
 
-var pak = L.geoJSON(pakAd0 , {}).addTo(map);
-var pakPro = L.geoJSON(pakPro , {}).addTo(map);
-var pakDis = L.geoJSON(pakDis , {}).addTo(map);
-var pakTeh = L.geoJSON(pakTeh , {}).addTo(map);
-// Real-time location
-var liveLocationEnabled = false;
-var liveLocationBuffer;
-var bufferLayer;
-var nearestHospitalMarker;
-
-if (!navigator.geolocation) {
-    console.log('Your browser does not support geolocation');
-} else {
-    // Create a button and add it to the map
-    var locationButton = L.control({ position: 'topleft' });
-
-    locationButton.onAdd = function (map) {
-        var buttonDiv = L.DomUtil.create('div', 'location-button');
-        buttonDiv.innerHTML = '<button onclick="fetchLiveLocation()">Live Location</button>';
-        return buttonDiv;
-    };
-
-    locationButton.addTo(map);
-}
-
-var pmarker;
-
-function fetchLiveLocation() {
-    navigator.geolocation.getCurrentPosition(geoposition);
-}
-
-function geoposition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-
-    // Create a popup content string
-    var popupContent = `Latitude: ${lat.toFixed(2)}<br>Longitude: ${lon.toFixed(2)}`;
-
-    // Display the information as a popup on the map
-    var popup = L.popup()
-        .setLatLng([lat, lon])
-        .setContent(popupContent);
-
-    if (pmarker) {
-        pmarker.setLatLng([lat, lon]);
-        pmarker.bindPopup(popup);
-    } else {
-        pmarker = L.marker([lat, lon]).addTo(map).bindPopup(popup);
-    }
-
-    if (liveLocationEnabled) {
-        // Clear previous buffer and hospital markers
-        if (bufferLayer) map.removeLayer(bufferLayer);
-        if (nearestHospitalMarker) map.removeLayer(nearestHospitalMarker);
-
-        // Apply buffer
-        var point = turf.point([lon, lat]);
-        var buffered = turf.buffer(point, liveLocationBuffer, { units: 'kilometers' });
-        bufferLayer = L.geoJSON(buffered).addTo(map);
-
-        // Find the nearest hospital among the ones within the buffer zone
-        var nearestHospital = findNearestHospital(L.latLng(lat, lon), Hospitals);
-
-        if (nearestHospital) {
-            nearestHospitalMarker = L.marker(nearestHospital.getLatLng(), {
-                icon: L.divIcon({
-                    className: 'nearest-hospital-icon',
-                    html: '<div>Nearest Hospital</div>'
-                })
-            }).addTo(map);
+var pakPro = L.geoJSON(pakPro, {
+    onEachFeature: function (feature, layer) {
+        if (feature.properties) {
+            layer.bindPopup(Object.keys(feature.properties)
+                .map(key => `<b>${key}:</b> ${feature.properties[key]}`)
+                .join('<br>'));
         }
-
-        // Iterate through points in GeoJSON file
-        Hospitals.eachLayer(function (layer) {
-            var popupContent = "<b>" + layer.feature.properties["Name"] + "</b><br>" + layer.feature.properties.Snippet;
-            layer.bindPopup(popupContent);
-        });
     }
-}
+});
 
-function findNearestHospital(realTimeLocation, hospitals) {
-    var nearestHospital;
-    var nearestDistance = Infinity;
-
-    hospitals.eachLayer(function (layer) {
-        var hospitalLocation = layer.getLatLng();
-        var distance = realTimeLocation.distanceTo(hospitalLocation);
-        if (distance < nearestDistance) {
-            nearestDistance = distance;
-            nearestHospital = layer;
+var pakDis = L.geoJSON(pakDis, {
+    onEachFeature: function (feature, layer) {
+        if (feature.properties) {
+            layer.bindPopup(Object.keys(feature.properties)
+                .map(key => `<b>${key}:</b> ${feature.properties[key]}`)
+                .join('<br>'));
         }
-    });
+    }
+});
 
-    return nearestHospital;
-}
+var pakTeh = L.geoJSON(pakTeh, {
+    onEachFeature: function (feature, layer) {
+        if (feature.properties) {
+            layer.bindPopup(Object.keys(feature.properties)
+                .map(key => `<b>${key}:</b> ${feature.properties[key]}`)
+                .join('<br>'));
+        }
+    }
+});
 
-var baseMaps = {
-    "Layer 1": layer1,
-    "OpenStreetMap": osm,
-    "OpenStreetMap.HOT": osmHOT
-};
-
+// Layer control
 var overlayMaps = {
     "Hospitals": Hospitals,
     "Pakistan": pak,
@@ -141,93 +80,11 @@ var overlayMaps = {
     "Tehsils": pakTeh
 };
 
-var bufferControl = L.control({ position: 'topleft' , collaps: 'false'});
-bufferControl.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'buffer-control');
-    div.innerHTML = '<h4>Buffer Zone</h4><select id="buffer-select"><option value="0">Select Buffer</option><option value="2">2km</option><option value="3">3km</option><option value="5">5km</option><option value="8">8km</option><option value="10">10km</option></select>';
-    L.DomEvent.disableClickPropagation(div);
-    return div;
-};
-bufferControl.addTo(map);
+var layerControl = L.control.layers({
+    "Layer 1": layer1,
+    "Esri World Imagery": Esri_WorldImagery,
+    "OpenStreetMap.HOT": osmHOT
+}, overlayMaps).addTo(map);
 
-document.getElementById('buffer-select').addEventListener('change', function () {
-    liveLocationBuffer = parseInt(this.value);
-    if (liveLocationBuffer > 0) {
-        liveLocationEnabled = true;
-        fetchLiveLocation();
-    } else {
-        liveLocationEnabled = false;
-        map.eachLayer(function (layer) {
-            if (layer instanceof L.Marker) {
-                map.removeLayer(layer);
-            }
-        });
-    }
-});
-
-// scale bar
-L.control.scale().addTo(map)
-
-var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-
-var routingControl; // Add this line to initialize routingControl
-
-function geoposition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-
-    // Create a popup content string
-    var popupContent = `Latitude: ${lat.toFixed(2)}<br>Longitude: ${lon.toFixed(2)}`;
-
-    // Display the information as a popup on the map
-    var popup = L.popup()
-        .setLatLng([lat, lon])
-        .setContent(popupContent);
-
-    if (pmarker) {
-        pmarker.setLatLng([lat, lon]);
-        pmarker.bindPopup(popup);
-    } else {
-        pmarker = L.marker([lat, lon]).addTo(map).bindPopup(popup);
-    }
-
-    if (liveLocationEnabled) {
-        // Clear previous buffer and hospital markers
-        if (bufferLayer) map.removeLayer(bufferLayer);
-        if (nearestHospitalMarker) map.removeLayer(nearestHospitalMarker);
-
-        // Apply buffer
-        var point = turf.point([lon, lat]);
-        var buffered = turf.buffer(point, liveLocationBuffer, { units: 'kilometers' });
-        bufferLayer = L.geoJSON(buffered).addTo(map);
-
-        // Find the nearest hospital among the ones within the buffer zone
-        var nearestHospital = findNearestHospital(L.latLng(lat, lon), Hospitals);
-
-        if (nearestHospital) {
-            nearestHospitalMarker = L.marker(nearestHospital.getLatLng(), {
-                icon: L.divIcon({
-                    className: 'nearest-hospital-icon',
-                    html: '<div>Nearest Hospital</div>'
-                })
-            }).addTo(map);
-
-            // Create the routing control
-            routingControl = L.Routing.control({
-                waypoints: [
-                    L.latLng(lat, lon), // Real-time location marker
-                    nearestHospitalMarker.getLatLng() // Nearest hospital marker
-                ],
-                routeWhileDragging: true,
-                lineOptions: {
-                    styles: [{color: 'Green', opacity: 0.5, weight: 6}]
-                },
-                createMarker: function(i, wp, nWps) {
-                    // Prevent creation of default markers
-                    return null;
-                }
-            }).addTo(map);
-        }
-    }
-}
+// Scale bar
+L.control.scale().addTo(map);
